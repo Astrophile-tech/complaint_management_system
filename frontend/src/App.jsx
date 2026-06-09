bb749b1fdee1af2b46d6181a329dc814f7fbb44e
NANDANA A S <nandanaas@NANDANAs-MacBook-Air.local>
2026-06-09 17:41:55 +0530

    complaint form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import MyComplaints from './components/MyComplaints';
 import ComplaintDetails from './components/ComplaintDetails';
 import ComplaintStatus from './components/ComplaintStatus';
+import AddComplaint from './components/AddComplaint';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={<MyComplaints />} />
       <Route path="/details" element={<ComplaintDetails />} />
       <Route path="/status" element={<ComplaintStatus />} />
+      <Route path="/add" element={<AddComplaint />} />
     </Routes>
   );
 }
